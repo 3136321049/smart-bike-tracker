f85edb08051d50a4136578c5794e4e7d12b83cc4
3136321049 <3136321049@qq.com>
2024-02-13 14:00:22 +0000

    website version 2

--- src/QAPage.js
+++ src/QAPage.js
@@ -1,10 +1,11 @@
 // QAPage.js
 import React from 'react';  
 import './QAPage.css';
+import background from './qa.jpg';
 
 function QAPage() {
   return (
-    <div className="qa-page">
+    <div className="qa-page" style={{ background: `url(${background})` }}>
       <h2>Questions and Answers</h2>
       <div className="qa-item">
         <p><strong>Q: How to get ID?</strong></p>
